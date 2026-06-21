3bc98731883ded268db7df36fe0d80c9376f5855
Afshin <xqu4t312x@gmail.com>
2026-06-21 08:52:59 +0330

    git commit -m "chore(vendor-mobile): fix release gradle config and rename app to golino seller"

--- apps/vendor_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/vendor_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import file_selector_macos
 import firebase_core
 import firebase_messaging
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
